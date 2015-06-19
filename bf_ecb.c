f597bfe97b4c29b09038b91af455baa7202b0624
kjh <kjh0922@ntreev.com>
2015-06-19 17:30:59 +0900

    s

--- bf_ecb.c
+++ bf_ecb.c
@@ -27,7 +27,7 @@
  *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ *    documentation and/or other materials provided with the distribution.ssss
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
  *    "This product includes cryptographic software written by
